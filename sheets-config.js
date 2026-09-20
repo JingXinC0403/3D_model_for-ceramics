d6332466f430f6f449e0ceae9b518d780006df92
Duck-quack-5966 <justin5966cct@gmail.com>
2026-09-20 11:46:08 +0800

    Delete sheets-config.js

--- sheets-config.js
+++
@@ -1,7 +0,0 @@
-/* ============================================================
-   sheets-config.js
-   ------------------------------------------------------------
-   Google Apps Script backend URL goes here after deployment.
-   ============================================================ */
-
-const CARE_API_URL = "https://script.google.com/macros/s/AKfycbyUynyUydpAAmFmFQAyajNb2O9Ui_bxdlrr6SeA0nhobuWKlTPcG-1g2UpfJ1DpKbTPfA/exec";
